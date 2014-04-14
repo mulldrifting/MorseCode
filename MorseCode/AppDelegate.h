4c31e37d4a8cde584ee5f9ab7ed8dad1f9c3cfcf
Lauren Lee <unrealeel@gmail.com>
2014-04-14 12:49:37 -0700

    Initial Commit

---
+++ MorseCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MorseCode
+//
+//  Created by Lauren Lee on 4/14/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MorseCode/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MorseCode
+//
+//  Created by Lauren Lee on 4/14/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
